2f3452788b3672bc1b6f9467b1274a8b4ee19d90
dubillaa477 <43178679+dubillaa477@users.noreply.github.com>
2018-09-20 14:39:21 -0600

    Updating projects

---
+++ Lab01.Logic/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Logic
+{
+    class Person
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(int id, string name, string lastname)
+        {
+            Id = id;
+            Name = name;
+            Lastname = lastname;
+        }
+    }
+}
--- lAB01.Console/Program.cs
+++ lAB01.Console/Program.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab01.Logic
+namespace lAB01.Console
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
